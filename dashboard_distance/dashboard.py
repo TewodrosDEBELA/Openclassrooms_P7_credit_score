77285fc0cceee634acbb4b6b9a70d5309f2b0294
Tewodros Cherenet DEBELA <99698850+TewodrosDEBELA@users.noreply.github.com>
2022-02-15 16:20:39 +0100

    Update dashboard.py

--- dashboard_distance/dashboard.py
+++ dashboard_distance/dashboard.py
@@ -26,7 +26,7 @@ def main() :
     @st.cache
     def load_data():
         z = ZipFile("application_train.zip")
-        data = pd.read_csv(z.open('application_train.csv'),index_col='SK_ID_CURR', encoding ='utf-8'))
+        data = pd.read_csv(z.open('application_train.csv'),index_col='SK_ID_CURR', encoding ='utf-8')
         z = ZipFile("X_test_final.zip")
         sample = pd.read_csv(z.open('X_test_final.csv', index_col='SK_ID_CURR', encoding ='utf-8'))
         description = pd.read_csv("features_description.csv", usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')             
