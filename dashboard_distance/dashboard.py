7207c12a14317bb73975423427e8ed74b43bcc1b
Tewodros Cherenet DEBELA <99698850+TewodrosDEBELA@users.noreply.github.com>
2022-02-15 13:03:24 +0100

    Update dashboard.py

--- dashboard_distance/dashboard.py
+++ dashboard_distance/dashboard.py
@@ -226,7 +226,7 @@ def main() :
     
     #Appel de l'API : 
     
-    #API_url = "http://127.0.0.1:5000/credit/" + str(chk_id)# Local
+    API_url = "http://127.0.0.1:5000/credit/" + str(chk_id)# Local
     
   
     with st.spinner('Chargement du score du client...'):
