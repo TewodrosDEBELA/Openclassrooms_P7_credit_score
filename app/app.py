ed8b26d44796bc740da719bd197efceefc436330
Tewodros Cherenet DEBELA <99698850+TewodrosDEBELA@users.noreply.github.com>
2022-02-16 14:31:05 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ from lightgbm import LGBMClassifier
 
 app = Flask(__name__)
 
-#API_url = "http://127.0.0.1:5000/credit/" 
+API_url = "http://127.0.0.1:5000/credit/" + str(chk_id)
 
 # tell Flask to use the above defined config
 
